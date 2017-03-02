6f60adbcef0352a41db01e6832ff097a64ade630
Sliim <sliim@mailoo.org>
2017-03-02 20:00:01 +0100

    Improved msf zsh plugin for existing envvars

--- files/default/zsh.d/plugins/msf.plugin.zsh
+++ files/default/zsh.d/plugins/msf.plugin.zsh
@@ -4,10 +4,10 @@
 # Recipe:: zsh
 # Plugin:: msf
 #
-export MSFRPC_HOST="127.0.0.1"
-export MSFRPC_PORT="55554"
-export MSFRPC_USER="msf"
-export MSFRPC_PASS="msf"
-export MSFRPC_TOKEN="msf"
-export MSFRPC_SSL=false
-export MSFRPC_URI=/api/
+test -e $MSFRPC_HOST && export MSFRPC_HOST="127.0.0.1"
+test -e $MSFRPC_PORT && export MSFRPC_PORT="55554"
+test -e $MSFRPC_USER && export MSFRPC_USER="msf"
+test -e $MSFRPC_PASS && export MSFRPC_PASS="msf"
+test -e $MSFRPC_TOKEN && export MSFRPC_TOKEN="msf"
+test -e $MSFRPC_SSL && export MSFRPC_SSL=false
+test -e $MSFRPC_URI && export MSFRPC_URI=/api/
