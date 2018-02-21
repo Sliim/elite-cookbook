b43f4b40e14365594eeb18077ecd0ced4a91a616
Sliim <sliim@mailoo.org>
2018-02-21 01:48:56 +0100

    Fix msf zsh envvars

--- files/default/zsh.d/plugins/msf.plugin.zsh
+++ files/default/zsh.d/plugins/msf.plugin.zsh
@@ -4,10 +4,10 @@
 # Recipe:: zsh
 # Plugin:: msf
 #
-test -e $MSFRPC_HOST && export MSFRPC_HOST="127.0.0.1"
-test -e $MSFRPC_PORT && export MSFRPC_PORT="55554"
-test -e $MSFRPC_USER && export MSFRPC_USER="msf"
-test -e $MSFRPC_PASS && export MSFRPC_PASS="msf"
-test -e $MSFRPC_TOKEN && export MSFRPC_TOKEN="msf"
-test -e $MSFRPC_SSL && export MSFRPC_SSL=false
-test -e $MSFRPC_URI && export MSFRPC_URI=/api/
+test -z "$MSFRPC_HOST" && export MSFRPC_HOST="127.0.0.1"
+test -z "$MSFRPC_PORT" && export MSFRPC_PORT="55554"
+test -z "$MSFRPC_USER" && export MSFRPC_USER="msf"
+test -z "$MSFRPC_PASS" && export MSFRPC_PASS="msf"
+test -z "$MSFRPC_TOKEN" && export MSFRPC_TOKEN="msf"
+test -z "$MSFRPC_SSL" && export MSFRPC_SSL=false
+test -z "$MSFRPC_URI" && export MSFRPC_URI=/api/
